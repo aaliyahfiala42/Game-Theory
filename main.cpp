e88585332129c110cc1375558fab9dcf7bd78488
Aaliyah Hänni <aaliyahhanni@gmail.com>
2024-02-10 10:41:39 -0800

    run N simulations for various algorithms

--- main.cpp
+++ main.cpp
@@ -1,16 +1,92 @@
+/********************************************8
+ * Game Theory Simulations
+ * 
+ * True (1): Cooperate
+ * False (0): Defect 
+**********************************************/
+
 #include <iostream>
-#include <vector>
-#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+bool Random(){
+    //randomly select true or false
+    return rand() % 2;
+}
+
+bool Cooperative(){
+    return true;
+}
+
+bool Defective(){
+    return false;
+}
+
+bool TitForTat(bool prev){
+    //initially cooperate
+    //o/w follow opponents previous choice
+    if ((!prev) || (prev == true)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+tuple<int, int> results(bool a, bool b){
+    int a_val = 0;
+    int b_val = 0;
+    //both cooperate
+    if (a && b) {
+        a_val = 10;
+        b_val = 10;
+    //a cooperates, b defects
+    } else if (a && !b) {
+        a_val = 0;
+        b_val = 5;
+    //a defects, b cooperates
+    } else if (!a && b) {
+        a_val = 5;
+        b_val = 0;
+    //both defect
+    } else {
+        a_val = 1;
+        b_val = 1;
+    }
+    return {a_val, b_val};
+}
+
+
 int main() 
 {
-    vector<string> msg {"Hello", "World!"};
+    //change val of random variable each iteration
+    srand(time(NULL));
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    //initialize player points
+    int A = 0; 
+    int B = 0;
+
+    //run simulation for N iterations
+    int N = 1000;
+    for (int i = 0; i < N; i++) {
+        bool prev;
+        //assign players algorithms
+        bool A_choice = Random();
+        bool B_choice = TitForTat(prev);
+        prev = A_choice;
+
+        tuple result = results(A_choice, B_choice);
+        
+        //update player scores
+        A += get<0>(result);
+        B += get<1>(result);
     }
-    cout << endl;
+
+    //output results
+    cout << "Total Points" << endl; 
+    cout << "A (Rand): " << A << endl;
+    cout << "B (T4T):  " << B << endl;
+
+    return 0;
 }
